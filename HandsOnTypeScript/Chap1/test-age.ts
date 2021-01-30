00470684d2e2fd460b00e52c13eae1e05bf70bec
DercilioFontes <derciliofontes@gmail.com>
2021-01-29 19:54:57 -0500

    Chap1: up to Object-oriented programming

---
+++ HandsOnTypeScript/Chap1/test-age.ts
@@ -0,0 +1,21 @@
+interface User {
+    name: string;
+    age: number;
+}
+
+function canDrive(user: User) {
+    console.log("user is ", user.name);
+
+    if (user.age >= 16) {
+        console.log("allow to drive");
+    } else {
+        console.log("do not allow to drive");
+    }
+}
+
+const tom: User = {
+    name: "tom",
+    age: 25
+}
+
+canDrive(tom);
